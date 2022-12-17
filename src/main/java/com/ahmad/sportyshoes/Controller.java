9415073c85c274c4e487a5074f18db1a65b8da02
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-12-17 22:25:22 +0300

    Initial commit

---
+++ src/main/java/com/ahmad/sportyshoes/Controller.java
@@ -0,0 +1,14 @@
+package com.ahmad.sportyshoes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    String Home() {
+        return "Hello World";
+    }
+
+}
